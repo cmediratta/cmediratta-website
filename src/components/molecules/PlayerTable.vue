b209d260784952304bdfc63a9b0c64862f4a5cb8
cmediratta <cole@mediratta.net>
2024-11-14 14:09:34 -0800

    actually adding the playertable/rowbar components

---
+++ src/components/molecules/PlayerTable.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import RowBar from './RowBar.vue'
+  defineProps({
+    win_percentages: {
+      type: Array,
+      required: true
+    }
+  })
+</script>
+
+<template>
+  <div class="leftalign">
+    <RowBar name="Player" description="Win Percentage" evenodd="even" topbottom="top"/>
+    <RowBar
+        v-for="(player,index) in win_percentages"
+        :name="player[0]"
+        :description="String(player[1] + '%')"
+        :evenodd="index % 2 === 1 ? 'even' : 'odd'"
+        :topbottom="index === win_percentages.length-1 ? 'bottom' : ''"
+      />
+  </div>
+</template>
---
+++ src/components/molecules/RowBar.vue
@@ -0,0 +1,57 @@
+<script setup>
+  defineProps({
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    evenodd: {
+      type: String,
+      required: true
+    },
+    topbottom: {
+      type: String,
+      required: false
+    }
+  })
+</script>
+
+<template>
+  <div :class="['row-bar', evenodd, topbottom]">
+    <div class="cell name">{{ name }}</div>
+    <div class="cell description">{{ description }}</div>
+  </div>
+</template>
+
+
+<style scoped>
+  .row-bar {
+    display: flex;
+    padding: 0.5vw;
+    border-bottom: 1px solid #ccc;
+    color: black;
+    width: 50vw;
+    font-size: 1.5vw;
+  }
+  .cell {
+    flex: 1;
+    padding: 0.5vh;
+  }
+  .even {
+    background-color: #f9f9f9;
+  }
+  .odd {
+    background-color: #ffffff;
+  }
+  .top {
+    border-top-left-radius: 2.5vh;
+    border-top-right-radius: 2.5vh;
+  }
+  .bottom {
+    border-bottom-left-radius: 2.5vh;
+    border-bottom-right-radius: 2.5vh;
+  }
+</style>
